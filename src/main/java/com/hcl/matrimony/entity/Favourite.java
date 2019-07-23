eec7cc9868d952486a8c9ed12b66502e386f7886
rksiet2 <rksiet2@gmail.com>
2019-07-23 15:17:40 +0530

    Favorite entity

--- src/main/java/com/hcl/matrimony/entity/Favourite.java
+++ src/main/java/com/hcl/matrimony/entity/Favourite.java
@@ -5,26 +5,29 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
-@Entity
+@Entity 
+@IdClass(FavouriteIdKey.class)
 @Table(name="favourite")
 public class Favourite {
 	
-	
+	@Id
 	@OneToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="user_id")
-	private Long userId;
+	private User userId;
 	
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinColumn(name="favourite_user_id")
-	private User favouriteUserId;
+	@Id
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="favorite_user_id")
+	private User favoriteUserId;
 	
 	@Column(name="updated_date")
 	private LocalDate updatedDate;
---
+++ src/main/java/com/hcl/matrimony/entity/FavouriteIdKey.java
@@ -0,0 +1,9 @@
+package com.hcl.matrimony.entity;
+
+import java.io.Serializable;
+
+public class FavouriteIdKey implements Serializable{
+
+	private User userId;
+	private User favoriteUserId;
+}

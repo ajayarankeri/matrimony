205d49d36ad1d3b01bc81e0cef00d6fcef30a746
ajayarankeri <ajayarankeri@gmail.com>
2019-07-23 18:09:08 +0530

    committed user controller test cases

--- src/main/java/com/hcl/matrimony/dto/LoginUserDto.java
+++ src/main/java/com/hcl/matrimony/dto/LoginUserDto.java
@@ -1,11 +1,15 @@
 package com.hcl.matrimony.dto;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class LoginUserDto {
-	
+	@NotNull
 	private String userName;
+	
+	@NotNull
 	private String password;
 
 }
